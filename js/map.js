b42858291f6fe2aface68f8a7f33d9316d2c2220
Sergey Volkov <serg.wulf.s@gmail.com>
2018-12-17 01:16:07 +0300

    Исправления. Часть2

--- js/map.js
+++ js/map.js
@@ -39,43 +39,15 @@ var listPhotos = [
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
-var realEstates = [];
-/*
-var realEstate = {
-  'author': {
-    'avatar': ''
-  },
-  'offer': {
-    'title': '',
-    'address': '',
-    'price': '',
-    'type': '',
-    'rooms': '',
-    'guests': '',
-    'checkin': '',
-    'checkout': '',
-    'features': '',
-    'description': '',
-    'photos': ''
-  },
-  'location': {
-    'x': '',
-    'y': ''
-  }
-};
-*/
 
-var randomLengthArray = function (array) {
-  var newArray = [];
-  var countElements = Math.round(Math.random() * array.length);
-  if (countElements === 0) {
-    countElements = 1;
-  }
-  for (var i = 0; i < countElements; i++) {
-    newArray[i] = array[i];
-  }
-  return newArray;
-};
+var typeResidence = {
+  'palace': 'Дворец',
+  'house': 'Дом',
+  'bungalo': 'Бунгало',
+  'flat': 'Квартира'
+}
+
+var realEstates = [];
 
 // Функция перемешивания массива, благополучно взятая из харбра, по совету, чтобы не изобретать велосипед :)
 var shuffle = function (arr) {
@@ -102,17 +74,10 @@ var getRandomElementOfArray = function (listElements) {
 
 // Функция, которая ищет в названии тип недвижимости и возвращает его в удобочитаемом виде
 var getTypeResidence = function (titleTypeResidence) {
-  if (titleTypeResidence.indexOf('дворец') !== -1) {
-    return 'palace';
-  }
-  if (titleTypeResidence.indexOf('домик') !== -1) {
-    return 'house';
-  }
-  if (titleTypeResidence.indexOf('квартира') !== -1) {
-    return 'flat';
-  }
-  if (titleTypeResidence.indexOf('бунгало') !== -1) {
-    return 'bungalo';
+  for (var key in typeResidence) {
+    if ((titleTypeResidence.toLowerCase().indexOf(typeResidence[key].toLowerCase())) !== -1) {
+      return key;
+    }
   }
   return 'unknown';
 };
@@ -125,10 +90,12 @@ var getPathImageAvatar = function (numberImage) {
 };
 
 var createRealEstates = function (count) {
-  var realEstate = {};
   var listRealEstate = [];
   for (var i = 0; i < count; i++) {
+    var realEstate = {};
+    realEstate['author'] = {};
     realEstate['author']['avatar'] = getPathImageAvatar(i + 1);
+    realEstate['offer'] = {};
     realEstate['offer']['title'] = titlesResidence[i];
     realEstate['offer']['price'] = getRandomNumberRange(MIN_PRICE, MAX_PRICE);
     realEstate['offer']['type'] = getTypeResidence(titlesResidence[i]);
@@ -136,18 +103,23 @@ var createRealEstates = function (count) {
     realEstate['offer']['guests'] = getRandomNumberRange(MIN_COUNT_GUESTS, MAX_COUNT_GUESTS);
     realEstate['offer']['checkin'] = listCheckInOut[getRandomElementOfArray(listCheckInOut)];
     realEstate['offer']['checkout'] = listCheckInOut[getRandomElementOfArray(listCheckInOut)];
-    realEstate['offer']['features'] = randomLengthArray(shuffle(listFeatures));
+    realEstate['offer']['features'] = shuffle(listFeatures).slice(Math.round(Math.random() * (listFeatures.length - 1)));
     realEstate['offer']['description'] = '';
     realEstate['offer']['photos'] = shuffle(listPhotos);
+    realEstate['location'] = {};
     realEstate['location']['x'] = getRandomNumberRange(MIN_COORDINATE_X, MAX_COORDINATE_X);
     realEstate['location']['y'] = getRandomNumberRange(MIN_COORDINATE_Y, MAX_COORDINATE_Y);
     realEstate['offer']['address'] = realEstate['location']['x'] + ', ' + realEstate['location']['y'];
-    listRealEstate.push(JSON.parse(JSON.stringify(realEstate)));
+    listRealEstate.push(realEstate);
   }
   return listRealEstate;
 };
 // Создание объектов JS на основе созданных данных
 realEstates = createRealEstates(COUNT_REAL_ESATE);
+
+for (var i = 0; i < COUNT_REAL_ESATE; i++) {
+  console.log(realEstates[i]['offer']['type']);
+}
 
 var mapAdverts = document.querySelector('.map');
 mapAdverts.classList.remove('map--faded');
@@ -180,47 +152,26 @@ blockPins.appendChild(renderPins(realEstates));
 // Создаем шаблон для отображения карточки объекта недвижимости
 var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
 
-// Функция перевода типа жилья на русский язык
-var getTranslateNameType = function (offerType) {
-  switch (offerType) {
-    case 'flat': return 'Квартира';
-    case 'house': return 'Дом';
-    case 'palace': return 'Дворец';
-    case 'bungalo': return 'Бунгало';
-    default: return 'unknown';
-
-  }
-};
-
 // Функция отображения карточки
 var renderCard = function (realEstateCard) {
   var cardElement = cardTemplate.cloneNode(true);
   cardElement.querySelector('.popup__title').textContent = realEstateCard['offer']['title'];
   cardElement.querySelector('.popup__text--address').textContent = realEstateCard['offer']['address'];
   cardElement.querySelector('.popup__text--price').innerHTML = realEstateCard['offer']['price'] + '&#x20bd;' + '<span>/ночь</span>';
-  cardElement.querySelector('.popup__type').textContent = getTranslateNameType(realEstateCard['offer']['type']);
+  cardElement.querySelector('.popup__type').textContent = typeResidence[realEstateCard['offer']['type']];
   cardElement.querySelector('.popup__text--capacity').textContent = realEstateCard['offer']['rooms'] + ' комнаты для ' + realEstateCard['offer']['guests'] + ' гостей';
   cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + realEstateCard['offer']['checkin'] + ', выездо до ' + realEstateCard['offer']['checkout'];
-  // В разметке есть все виды услуг. В массиве могут быть не все
-  // Чтобы отобразить нужные, необходимо перебрать элементы в разметке,
-  // если в разметке есть элемент, и такой элемент есть в массиве, то оставляем пункт разметки
-  // если в разметке есть элемент, а в массиве нет, то удаляем узел разметки
+  // В разметке находим блок предоставления услуг
   var popupFeatures = cardElement.querySelector('.popup__features');
-  var deleteFeatures = [];
-  for (var indexOne = 0; indexOne < popupFeatures.children.length; indexOne++) {
-    var isFeature = false;
-    for (var indexTwo = 0; indexTwo < realEstateCard['offer']['features'].length; indexTwo++) {
-      if ((popupFeatures.children[indexOne].className.indexOf('--' + realEstateCard['offer']['features'][indexTwo])) !== -1) {
-        isFeature = true;
-      }
-    }
-    if (!isFeature) {
-      deleteFeatures.push(popupFeatures.children[indexOne]);
-    }
+  // Удаляем все виды услуг из разметки
+  while (popupFeatures.firstChild) {
+    popupFeatures.removeChild(popupFeatures.firstChild);
   }
-  // Удаляем ненужные виды услуг
-  for (var indexDeleteFeature = 0; indexDeleteFeature < deleteFeatures.length; indexDeleteFeature++) {
-    popupFeatures.removeChild(deleteFeatures[indexDeleteFeature]);
+  // Добавляем нужные услуги в разметку
+  for (var j = 0; j < realEstateCard['offer']['features'].length; j++) {
+    var elementFeature = document.createElement('li');
+    elementFeature.className = 'popup__feature popup__feature--' + realEstateCard['offer']['features'][j];
+    popupFeatures.appendChild(elementFeature);
   }
   cardElement.querySelector('.popup__description').textContent = realEstateCard['offer']['description'];
   // Добавляем фотографии в карточку объекта недвижимости
